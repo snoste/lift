88153fe22ccb2995c5c47b397fb0facbd0cae91c
snoste <snorriste@gmail.com>
2016-10-24 17:27:13 +0200

    jhonny

--- bool.sh
+++ bool.sh
@@ -1,3 +1,3 @@
 mcrl22lps project.mcrl2 project.lps
-lps2pbes project.lps --formula=Requirements.mcf project.pbes
+lps2pbes project.lps --formula=requirement.m project.pbes
 pbes2bool project.pbes
--- requirement.m
+++ requirement.m
@@ -1,57 +1,58 @@
+%------------------------------------------------------------------- TRUE
 [(!gateState1(false))*.openGate(G0)]false &&
 [true*.openGate(G1).(!gateState1(false))*.openGate(G0)]false &&
 [!(gateState0(false))*.openGate(G1)]false &&
-[true*.openGate(G0).!gateState0(false))*.openGate(G1)]false &&
+[true*.openGate(G0).(!gateState0(false))*.openGate(G1)]false &&
 
-%-------------------------------------------------------------------
+%------------------------------------------------------------------- TRUE
 [!(valveState1(false))*.openValve(V0)]false &&
 [true*.openValve(V1).!(valveState1(false))*.openValve(V0)]false &&
 [!(valveState1(false))*.openValve(V0)]false &&
 [true*.openValve(V1).!(valveState1(false))*.openValve(V0)]false &&
 
-%-------------------------------------------------------------------
+%------------------------------------------------------------------- TRUE
 [!(valveState1(false))*.openGate(G0)]false &&
 [true*.openValve(V1).!(valveState1(false))*.openGate(G0)]false &&
 [!(valveState0(false))*.openGate(G1)]false &&
 [true*.openValve(V0).!(valveState0(false))*.openGate(G1)]false &&
 
-%-------------------------------------------------------------------
-[!(gateState1(false))*.openValve(V0)]false &&
-[true*.openGate(G1).!(gateState1(false))*.openValve(V0)]false &&
-[!(gateState0(false))*.openValve(V1)]false &&
-[true*.openGate(G0).!(gateState0(false))*.openValve(V1)]false &&
+%------------------------------------------------------------------- FALSE
+%[!(gateState1(false))*.openValve(V0)]false &&
+%[true*.openGate(G1).(!gateState1(false))*.openValve(V0)]false &&
+%[!(gateState0(false))*.openValve(V1)]false &&
+%[true*.openGate(G0).(!gateState0(false))*.openValve(V1)]false
 
-%-------------------------------------------------------------------
+%------------------------------------------------------------------- TRUE
 [true*.!(gateSensor0(false)).closeGate(G0)]false &&
 [true*.!(gateSensor1(false)).closeGate(G1)]false &&
 
-%-------------------------------------------------------------------
-[true*.!(compareWaterLevel0(true)).openGate(G0)]false &&
-[true*.!(compareWaterLevel1(true)).openGate(G1)]false &&
+%------------------------------------------------------------------- FALSE
+%[true*.!(compareWaterLevel0(true)).openGate(G0)]false &&
+%[true*.!(compareWaterLevel1(true)).openGate(G1)]false &&
 
-%-------------------------------------------------------------------
+%------------------------------------------------------------------- TRUE
 [true*.passSignalOn(S00).!(haltSignalOff(S00))]false &&
 [true*.passSignalOn(S01).!(haltSignalOff(S01))]false &&
 [true*.passSignalOn(S10).!(haltSignalOff(S10))]false &&
 [true*.passSignalOn(S11).!(haltSignalOff(S11))]false &&
 
-%-------------------------------------------------------------------
+%------------------------------------------------------------------- TRUE
 [true*.haltSignalOn(S00).!(passSignalOff(S00))]false &&
 [true*.haltSignalOn(S01).!(passSignalOff(S01))]false &&
 [true*.haltSignalOn(S10).!(passSignalOff(S10))]false &&
 [true*.haltSignalOn(S11).!(passSignalOff(S11))]false &&
 
-%-------------------------------------------------------------------
+%------------------------------------------------------------------- TRUE
 [true*.!(haltSignalOn(S00)).passSignalOff(S00)]false &&
 [true*.!(haltSignalOn(S01)).passSignalOff(S01)]false &&
 [true*.!(haltSignalOn(S10)).passSignalOff(S10)]false &&
 [true*.!(haltSignalOn(S11)).passSignalOff(S11)]false &&
 
-%-------------------------------------------------------------------
+%------------------------------------------------------------------- TRUE
 [true*.!(passSignalOn(S00)).haltSignalOff(S00)]false &&
 [true*.!(passSignalOn(S01)).haltSignalOff(S01)]false &&
 [true*.!(passSignalOn(S10)).haltSignalOff(S10)]false &&
-[true*.!(passSignalOn(S11)).haltSignalOff(S11)]false &&
+[true*.!(passSignalOn(S11)).haltSignalOff(S11)]false
 
 %-------------------------------------------------------------------
 %!(gateState1(true))*.passSignalOn(2)]false &&
